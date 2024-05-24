15d54f18ff2c93ce53b02e12ad9296138a90fb28
Gustavo Larsen <guslarsen@gmail.com>
2024-05-23 21:57:39 -0300

    melhorias de estilo e adicao do imposto de renda

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ function calculateCompoundInterest() {
   const principal = parseFloat(document.getElementById('principal').value);
   const rate = parseFloat(document.getElementById('rate').value) / 100;
   const time = parseInt(document.getElementById('time').value);
+  const taxRate = parseFloat(document.getElementById('tax').value) / 100;
 
   // Calcula o valor futuro
   const futureValue = principal * Math.pow(1 + rate, time);
 
-  // Exibe o resultado
+  // Calcula o rendimento
+  const earnings = futureValue - principal;
+
+  // Calcula o imposto de renda
+  const tax = earnings * taxRate;
+
+  // Calcula o valor lucro após o desconto do imposto de renda
+  const netPrincipal = futureValue - tax;
+
+  // Exibe os resultados
   document.getElementById('result').innerText = futureValue.toFixed(2);
+  document.getElementById('earnings').innerText = earnings.toFixed(2);
+  document.getElementById('ir').innerText = tax.toFixed(2);
+  document.getElementById('net-principal').innerText = netPrincipal.toFixed(2);
 }
